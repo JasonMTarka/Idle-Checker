bc44afb0a46e3dca8ebc053f5635e18364f76fdb
Jason Tarka <jasonmtarka@gmail.com>
2021-06-13 13:17:41 +0900

    Resource Check Update
    - Improved resource checking algorithm
    
    - General refactoring

--- idle_checker.py
+++ idle_checker.py
@@ -17,41 +17,41 @@ class Idle_Checker:
             logger.setLevel(logging.INFO)
 
             formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
-            file_handler = logging.FileHandler('idle_checker.log')
+            file_handler = logging.FileHandler('idle_checker_logs.log')
             file_handler.setFormatter(formatter)
+
+            logger.addHandler(file_handler)
 
             if self.debug:
                 stream_handler = logging.StreamHandler()
                 stream_handler.setFormatter(formatter)
-
-            logger.addHandler(file_handler)
-            logger.addHandler(stream_handler)
+                logger.addHandler(stream_handler)
 
             return logger
 
-        def debug_mode() -> None:
-            # Sets constants to lower levels for debugging purposes
-
-            self.RUNNING_DURATION = 30
-            self.CPU_THRESHOLD = 10
-            self.SLEEP_MODE_LENGTH = 10
-            self.PRESENCE_WAIT_TIME = 2
-            self.PRESENCE_CHECK_COUNT = 3
-
         self.debug = kwargs.get("debug")
         self.logger = logger_setup()
-
-        self.ELAPSED_TIME = 0  # Rough elapsed time the program has been running
-        self.RUNNING_DURATION = 60 * 60 * 4  # Total running length of program - last digit is # of hours
-        self.CPU_THRESHOLD = 30  # Maximum for acceptable CPU usage, in %
-        self.MEMORY_THRESHOLD = 50  # Maximum for acceptable RAM usage, in %
-        self.SLEEP_MODE_LENGTH = 600  # 600 seconds = 10 minutes; time during Sleep Mode between resource check
-        self.PRESENCE_WAIT_TIME = 6  # Time between checks when checking user's presence
-        self.PRESENCE_CHECK_COUNT = 60  # Number of checks for user presence; with wait time of 6 seconds, 10 checks equals 1 minute
         self.running = True
 
-        if self.debug:
-            debug_mode()
+        self.ELAPSED_TIME = 0  # Rough elapsed time the program has been running
+        self.RUNNING_DURATION = 60 * 60 * 4  # Total allowed running length of program - last digit is # of hours
+        self.SLEEP_MODE_LENGTH = 600  # 600 seconds = 10 minutes; time during Sleep Mode between resource check
+
+        self.CPU_THRESHOLD = 30  # Maximum for acceptable CPU usage, in %
+        self.MEMORY_THRESHOLD = 50  # Maximum for acceptable RAM usage, in %
+        self.RESOURCE_CHECKS = 3  # Number of resource checks required to fail or pass to determine heavy usage
+        self.RESOURCE_CHECK_INTERVAL = 3  # Number of seconds between resource checks
+        self.MAXIMUM_RESOURCE_CHECKS = 10  # Failsafe value in case checks keep rebounding between active and inactive
+
+        self.PRESENCE_WAIT_TIME = 6  # Number of seconds between presence checks
+        self.PRESENCE_CHECK_COUNT = 60  # Number of checks for user presence; with wait time of 6 seconds, 10 checks equals 1 minute
+
+        if self.debug:  # Sets some constant values lower for debugging purposes
+            self.RUNNING_DURATION = 30
+            self.CPU_THRESHOLD = 10
+            self.SLEEP_MODE_LENGTH = 5
+            self.PRESENCE_WAIT_TIME = 2
+            self.PRESENCE_CHECK_COUNT = 5
 
     def main(self) -> None:
 
@@ -62,7 +62,6 @@ class Idle_Checker:
             sleep(self.SLEEP_MODE_LENGTH)
 
         while self.running and self.ELAPSED_TIME <= self.RUNNING_DURATION:
-            self.update_resources()
             if self.resource_utilization():  # Checks if resources are being heavily utilized
                 if self.presence():  # Checks if user is present
                     sleep_mode()
@@ -80,25 +79,48 @@ class Idle_Checker:
 
     def resource_utilization(self) -> bool:
 
-        if self.cpu >= self.CPU_THRESHOLD or self.memory >= self.MEMORY_THRESHOLD:
-            self.logger.info(f"Resources are being heavily utilized. (Maximum CPU usage allowed: {self.CPU_THRESHOLD}%, Maximum RAM usage allowed: {self.MEMORY_THRESHOLD}%)")
+        resource_counter = 0
+        total_checks = 0
+
+        while resource_counter < self.RESOURCE_CHECKS and resource_counter > -self.RESOURCE_CHECKS and total_checks < self.MAXIMUM_RESOURCE_CHECKS:
+
+            sleep(self.RESOURCE_CHECK_INTERVAL)
+            self.update_resources()
+
+            if self.cpu >= self.CPU_THRESHOLD or self.memory >= self.MEMORY_THRESHOLD:
+                self.logger.info(f"Resources are being heavily utilized. (Maximum CPU usage allowed: {self.CPU_THRESHOLD}%, Maximum RAM usage allowed: {self.MEMORY_THRESHOLD}%)")
+                resource_counter += 1
+                total_checks += 1
+
+            else:
+                self.logger.info(f"Resources not being heavily utilized. (Maximum CPU usage allowed: {self.CPU_THRESHOLD}%, Maximum RAM usage allowed: {self.MEMORY_THRESHOLD}%)")
+                resource_counter -= 1
+                total_checks += 1
+
+        if resource_counter >= self.RESOURCE_CHECKS:  # Heavy resource usage
+            self.logger.warning(f"Computer has failed resource checks. (Total number of checks: {total_checks})")
             return True
-        else:
-            self.logger.info(f"Resources not being heavily utilized. (Maximum CPU usage allowed: {self.CPU_THRESHOLD}%, Maximum RAM usage allowed: {self.MEMORY_THRESHOLD}%)")
+        elif resource_counter <= -self.RESOURCE_CHECKS:  # Light resource usage
+            self.logger.info(f"Computer has passed resource checks. (Total number of checks: {total_checks})")
+            return False
+        elif total_checks >= self.MAXIMUM_RESOURCE_CHECKS:  # Total number of checks exceeded
+            self.logger.warning(f"Computer has reached maximum number of allowed checks. (Total number of checks: {total_checks}")
+            return False
+        else:  # Other error catcher
+            self.logger.error(f"An unknown error has occured in resource checks. (Total number of checks: {total_checks})")
             return False
 
     def presence(self) -> bool:
 
         mouse_x, mouse_y = pyautogui.position()
-        sleep(self.PRESENCE_WAIT_TIME)
 
         for _ in range(self.PRESENCE_CHECK_COUNT):
+            sleep(self.PRESENCE_WAIT_TIME)
             self.logger.info("Checking for user presence...")
             mouse_new_x, mouse_new_y = pyautogui.position()
             if mouse_new_x != mouse_x or mouse_new_y != mouse_y:
-                self.logger.info("User is present.")
+                self.logger.info("Activity detected.")
                 return True
-            sleep(self.PRESENCE_WAIT_TIME)
 
         self.logger.info("User does not seem to be present.")
         return False
@@ -125,6 +147,7 @@ class Idle_Checker:
 
 
 def main():
+
     debug = False
     for cl_arg in sys.argv[1:]:
         if cl_arg == "--debug" or cl_arg == "-d":
