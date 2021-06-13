b48ff7e9442c92c43cd2c2a4ee787f461b5c8bb1
Jason Tarka <jasonmtarka@gmail.com>
2021-06-13 10:56:09 +0900

    Debug and Logging Update
    - Added logging
    
    - Added support for -d and --debug CLI arguments

--- idle_checker.py
+++ idle_checker.py
@@ -1,13 +1,45 @@
 import os
+import sys
 import psutil
 import pyautogui
 import boto3
+import logging
 from time import sleep
 
 
 class Idle_Checker:
 
-    def __init__(self) -> None:
+    def __init__(self, **kwargs) -> None:
+
+        def logger_setup() -> logging.Logger:
+
+            logger = logging.getLogger(__name__)
+            logger.setLevel(logging.INFO)
+
+            formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+            file_handler = logging.FileHandler('idle_checker.log')
+            file_handler.setFormatter(formatter)
+
+            if self.debug:
+                stream_handler = logging.StreamHandler()
+                stream_handler.setFormatter(formatter)
+
+            logger.addHandler(file_handler)
+            logger.addHandler(stream_handler)
+
+            return logger
+
+        def debug_mode() -> None:
+            # Sets constants to lower levels for debugging purposes
+
+            self.RUNNING_DURATION = 30
+            self.CPU_THRESHOLD = 10
+            self.SLEEP_MODE_LENGTH = 10
+            self.PRESENCE_WAIT_TIME = 2
+            self.PRESENCE_CHECK_COUNT = 3
+
+        self.debug = kwargs.get("debug")
+        self.logger = logger_setup()
 
         self.ELAPSED_TIME = 0  # Rough elapsed time the program has been running
         self.RUNNING_DURATION = 60 * 60 * 4  # Total running length of program - last digit is # of hours
@@ -18,13 +50,16 @@ class Idle_Checker:
         self.PRESENCE_CHECK_COUNT = 60  # Number of checks for user presence; with wait time of 6 seconds, 10 checks equals 1 minute
         self.running = True
 
+        if self.debug:
+            debug_mode()
+
     def main(self) -> None:
 
         def sleep_mode() -> None:
 
-            print(f"Entering sleep mode... ({self.SLEEP_MODE_LENGTH} seconds)")
-            sleep(self.SLEEP_MODE_LENGTH)
+            self.logger.info(f"Entering sleep mode... ({self.SLEEP_MODE_LENGTH} seconds)")
             self.ELAPSED_TIME += self.SLEEP_MODE_LENGTH
+            sleep(self.SLEEP_MODE_LENGTH)
 
         while self.running and self.ELAPSED_TIME <= self.RUNNING_DURATION:
             self.update_resources()
@@ -36,20 +71,20 @@ class Idle_Checker:
             else:
                 sleep_mode()
 
-        print("Closing program...")
+        self.logger.info("Closing program...")
 
     def update_resources(self) -> None:
 
         self.cpu, self.memory = (psutil.cpu_percent(interval=0.6), psutil.virtual_memory().percent)
-        print(f"CPU usage is at {self.cpu}% and memory usage is at {self.memory}%.")
+        self.logger.info(f"CPU usage is at {self.cpu}% and memory usage is at {self.memory}%.")
 
     def resource_utilization(self) -> bool:
 
         if self.cpu >= self.CPU_THRESHOLD or self.memory >= self.MEMORY_THRESHOLD:
-            print(f"Resources are being heavily utilized. (Maximum CPU usage allowed: {self.CPU_THRESHOLD}%, Maximum RAM usage allowed: {self.MEMORY_THRESHOLD}%)")
+            self.logger.info(f"Resources are being heavily utilized. (Maximum CPU usage allowed: {self.CPU_THRESHOLD}%, Maximum RAM usage allowed: {self.MEMORY_THRESHOLD}%)")
             return True
         else:
-            print(f"Resources not being heavily utilized. (Maximum CPU usage allowed: {self.CPU_THRESHOLD}%, Maximum RAM usage allowed: {self.MEMORY_THRESHOLD}%)")
+            self.logger.info(f"Resources not being heavily utilized. (Maximum CPU usage allowed: {self.CPU_THRESHOLD}%, Maximum RAM usage allowed: {self.MEMORY_THRESHOLD}%)")
             return False
 
     def presence(self) -> bool:
@@ -58,35 +93,46 @@ class Idle_Checker:
         sleep(self.PRESENCE_WAIT_TIME)
 
         for _ in range(self.PRESENCE_CHECK_COUNT):
+            self.logger.info("Checking for user presence...")
             mouse_new_x, mouse_new_y = pyautogui.position()
             if mouse_new_x != mouse_x or mouse_new_y != mouse_y:
-                print("User is present.")
+                self.logger.info("User is present.")
                 return True
             sleep(self.PRESENCE_WAIT_TIME)
 
-        print("User does not seem to be present.")
+        self.logger.info("User does not seem to be present.")
         return False
 
     def send_notification(self) -> None:
 
-        print("Sending notification via AWS SNS...")
+        self.logger.info("Sending notification via AWS SNS...")
 
-        client = boto3.client(
-            "sns",
-            aws_access_key_id=os.environ.get("AWS-Python-Access-Key-ID"),
-            aws_secret_access_key=os.environ.get("AWS-Python-Secret-Access-Key"),
-            region_name=os.environ.get("AWS-Region")
-        )
+        if self.debug is False:
+            client = boto3.client(
+                "sns",
+                aws_access_key_id=os.environ.get("AWS-Python-Access-Key-ID"),
+                aws_secret_access_key=os.environ.get("AWS-Python-Secret-Access-Key"),
+                region_name=os.environ.get("AWS-Region")
+            )
 
-        client.publish(
-            TopicArn=os.environ.get("AWS-Python-Idle-Checker-TopicArn"),
-            Message=f"Your CPU usage was recorded at {self.cpu}% and your RAM usage was recorded at {self.memory}%.  Did you leave a task running?",
-            Subject="Idle Checker Notification",
-        )
+            client.publish(
+                TopicArn=os.environ.get("AWS-Python-Idle-Checker-TopicArn"),
+                Message=f"Your CPU usage was recorded at {self.cpu}% and your RAM usage was recorded at {self.memory}%.  Did you leave a task running?",
+                Subject="Idle Checker Notification",
+            )
 
         self.running = False
 
 
-if __name__ == "__main__":
-    checker = Idle_Checker()
+def main():
+    debug = False
+    for cl_arg in sys.argv[1:]:
+        if cl_arg == "--debug" or cl_arg == "-d":
+            debug = True
+
+    checker = Idle_Checker(debug=debug)
     checker.main()
+
+
+if __name__ == "__main__":
+    main()
