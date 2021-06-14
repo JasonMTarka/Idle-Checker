cb2c50c28b8555546112dc34ab8fae3bc33d3d96
Jason Tarka <jasonmtarka@gmail.com>
2021-06-14 13:37:43 +0900

    Duration Update
    - Changed program so that three passed resource checks while user is away will terminate the program
    
    - Changed some default values to avoid accidental triggering of notification
    
    - Clarified some comments, changed presence log level

--- idle_checker.py
+++ idle_checker.py
@@ -7,7 +7,7 @@ import logging
 from time import sleep
 
 
-class Idle_Checker:
+class Idle_Usage_Checker:
 
     def __init__(self, **kwargs) -> None:
 
@@ -17,7 +17,7 @@ class Idle_Checker:
             logger.setLevel(logging.INFO)
 
             formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
-            file_handler = logging.FileHandler('idle_checker_logs.log')
+            file_handler = logging.FileHandler('idle_usage_checker_logs.log')
             file_handler.setFormatter(formatter)
 
             logger.addHandler(file_handler)
@@ -34,17 +34,18 @@ class Idle_Checker:
         self.running = True
 
         self.ELAPSED_TIME = 0  # Rough elapsed time the program has been running
-        self.RUNNING_DURATION = 60 * 60 * 4  # Total allowed running length of program - last digit is # of hours
+        self.RUNNING_DURATION = 60 * 60 * 4  # (seconds * minutes * hours) Total allowed running length of program; incremented by sleep_mode()
         self.SLEEP_MODE_LENGTH = 900  # 900 seconds = 15 minutes; duration of Sleep Mode between resource check
 
         self.CPU_THRESHOLD = 30  # Maximum for acceptable CPU usage, in %
-        self.MEMORY_THRESHOLD = 50  # Maximum for acceptable RAM usage, in %
+        self.MEMORY_THRESHOLD = 55  # Maximum for acceptable RAM usage, in %
         self.RESOURCE_CHECKS = 3  # Number of resource checks required to fail or pass to determine heavy usage
         self.RESOURCE_CHECK_INTERVAL = 3  # Number of seconds between resource checks
         self.MAXIMUM_RESOURCE_CHECKS = 10  # Failsafe value in case checks keep rebounding between active and inactive
+        self.MAXIMUM_PASSED_CHECKS = 3  # Number of passed resource checks allowed before terminating program
 
-        self.PRESENCE_WAIT_TIME = 6  # Number of seconds between presence checks
-        self.PRESENCE_CHECK_COUNT = 150  # Number of checks for user presence; with wait time of 6 seconds, 10 checks = 1 minute
+        self.PRESENCE_WAIT_TIME = 60  # Number of seconds between presence checks
+        self.PRESENCE_CHECK_COUNT = 15  # Number of checks for user presence; with PRESENCE_WAIT_TIME of 60, 15 checks = 15 minutes
 
         if self.debug:  # Sets some constant values lower for debugging purposes
             self.RUNNING_DURATION = 30
@@ -64,14 +65,18 @@ class Idle_Checker:
             sleep(self.SLEEP_MODE_LENGTH)
 
         self.logger.info("Beginning main loop.")
+        total_passed_resource_checks = 0
 
-        while self.running and self.ELAPSED_TIME <= self.RUNNING_DURATION:
-            if not self.presence():  # Checks if user is present
+        while self.running and self.ELAPSED_TIME <= self.RUNNING_DURATION and total_passed_resource_checks < self.MAXIMUM_PASSED_CHECKS:
+            self.logger.info("Checking for user presence...")
+            if not self.presence():  # Checks if user is present (self.presence returns True if present)
                 if self.resource_utilization():  # Checks resource utilization
                     self.send_notification()  # If resources are being utilized and user is not present, AWS SNS sends a notification email and ends the loop
                 else:
+                    total_passed_resource_checks += 1
                     sleep_mode()
             else:
+                total_passed_resource_checks = 0  # Resets number of passed resorce checks if user presence is detected
                 sleep_mode()
 
         self.logger.info("Closing program...")
@@ -101,10 +106,10 @@ class Idle_Checker:
                 resource_counter -= 1
                 total_checks += 1
 
-        if resource_counter >= self.RESOURCE_CHECKS:  # Heavy resource usage
+        if resource_counter >= self.RESOURCE_CHECKS:  # Case of heavy resource usage
             self.logger.warning(f"Computer has failed resource checks. (Total number of checks: {total_checks})")
             return True
-        elif resource_counter <= -self.RESOURCE_CHECKS:  # Light resource usage
+        elif resource_counter <= -self.RESOURCE_CHECKS:  # Case of light resource usage
             self.logger.info(f"Computer has passed resource checks. (Total number of checks: {total_checks})")
             return False
         elif total_checks >= self.MAXIMUM_RESOURCE_CHECKS:  # Total number of checks exceeded
@@ -120,7 +125,7 @@ class Idle_Checker:
 
         for _ in range(self.PRESENCE_CHECK_COUNT):
             sleep(self.PRESENCE_WAIT_TIME)
-            self.logger.info("Checking for user presence...")
+            self.logger.debug("Checking for user presence...")
             mouse_new_x, mouse_new_y = win32api.GetCursorPos()
             if mouse_new_x != mouse_x or mouse_new_y != mouse_y:
                 self.logger.info("Activity detected.")
@@ -157,7 +162,7 @@ def main():
         if cl_arg == "--debug" or cl_arg == "-d":
             debug = True
 
-    checker = Idle_Checker(debug=debug)
+    checker = Idle_Usage_Checker(debug=debug)
     checker.main()
 
 
